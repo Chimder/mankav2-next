2539ee59ba9b5ed588c47f111d2c5088ac336a65
Chimder <dimas.kirilyuk@gmail.com>
2024-10-30 20:19:58 +0200

    rewrites proxy

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,18 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  // reactStrictMode: true,
   experimental: {
     reactCompiler: true,
   },
-  // reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/proxy-api/:path*',
+        destination: 'https://api.mangadex.org/:path*',
+      },
+    ]
+  },
 }
 
 export default nextConfig
