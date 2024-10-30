7b96cb3fc85e2a6ea4d3fe566111953c5643f10c
Chimder <dimas.kirilyuk@gmail.com>
2024-10-30 22:10:32 +0200

    4

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: 'https://api.mangadex.org:path*',
+        source: 'https://api.mangadex.org/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
