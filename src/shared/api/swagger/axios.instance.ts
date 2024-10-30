027f791365f1f73c76559acb5acde1da6bb01629
Chimder <dimas.kirilyuk@gmail.com>
2024-10-30 21:29:13 +0200

    User-Agent

--- src/shared/api/swagger/axios.instance.ts
+++ src/shared/api/swagger/axios.instance.ts
@@ -3,10 +3,11 @@ import Axios, { AxiosError, AxiosRequestConfig } from 'axios'
 export const AXIOS_INSTANCE = Axios.create({
   // baseURL: 'https://api.mangadex.org',
   baseURL: '/proxy-api',
-  // headers: {
-  //   'Content-Type': 'application/json',
-  //   'Accept': '*/*',
-  // },
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': '*/*',
+    'User-Agent': 'Manka/1.0',
+  },
 })
 
 interface CancellablePromise<T> extends Promise<T> {
