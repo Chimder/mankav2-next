a8f60e20bf052038540030f9b73f2bb6a5b2805f
Chimder <dimas.kirilyuk@gmail.com>
2024-10-31 10:51:52 +0200

    13

--- next.config.ts
+++ next.config.ts
@@ -1,40 +1,20 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   experimental: {
     reactCompiler: true,
   },
-  // async headers() {
-  //   return [
-  //     {
-  //       source: '/(.*)',
-  //       headers: [
-  //         { key: 'Access-Control-Allow-Credentials', value: 'true' },
-  //         {
-  //           key: 'Access-Control-Allow-Origin',
-  //           value: 'https://api.mangadex.org',
-  //         },
-  //         {
-  //           key: 'Access-Control-Allow-Methods',
-  //           value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-  //         },
-  //         {
-  //           key: 'Access-Control-Allow-Headers',
-  //           value: 'Authorization, Content-Type, Accept',
-  //         },
-  //       ],
-  //     },
-  //   ]
-  // },
-  // async rewrites() {
-  //   return {
-  //     beforeFiles: [
-  //       {
-  //         source: '/proxy-api/:path*',
-  //         destination: 'https://api.mangadex.org/:path*',
-  //       },
-  //     ],
-  //   }
-  // },
+  async rewrites() {
+    if (!isProd) {
+      return [
+        {
+          source: '/proxy-api/:path*',
+          destination: 'https://api.mangadex.org/:path*',
+        },
+      ]
+    }
+    return []
+  },
 }
 
 module.exports = nextConfig
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -12,7 +12,7 @@ function SearchManga() {
         {mangas?.data?.map(manga => (
           <Link href={`title/${manga.id}`} key={manga.id}>
             <img
-              src={`https://mangadex.org/covers/${manga.id}/${manga.relationships?.find(obj => obj.type === 'cover_art')?.attributes?.fileName}`}
+              src={`/api/proxy?url=https://mangadex.org/covers/${manga.id}/${manga.relationships?.find(obj => obj.type === 'cover_art')?.attributes?.fileName}`}
               width={330}
               height={210}
               alt=""
