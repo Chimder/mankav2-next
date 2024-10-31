7232e83991f2bf652cb502e914598b0bc2e73a92
Chimder <dimas.kirilyuk@gmail.com>
2024-10-31 10:39:02 +0200

    m2

--- next.config.ts
+++ next.config.ts
@@ -3,38 +3,38 @@ const nextConfig = {
   experimental: {
     reactCompiler: true,
   },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: 'https://api.mangadex.org',
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'Authorization, Content-Type, Accept',
-          },
-        ],
-      },
-    ]
-  },
-  async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: '/proxy-api/:path*',
-          destination: 'https://api.mangadex.org/:path*',
-        },
-      ],
-    }
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/(.*)',
+  //       headers: [
+  //         { key: 'Access-Control-Allow-Credentials', value: 'true' },
+  //         {
+  //           key: 'Access-Control-Allow-Origin',
+  //           value: 'https://api.mangadex.org',
+  //         },
+  //         {
+  //           key: 'Access-Control-Allow-Methods',
+  //           value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+  //         },
+  //         {
+  //           key: 'Access-Control-Allow-Headers',
+  //           value: 'Authorization, Content-Type, Accept',
+  //         },
+  //       ],
+  //     },
+  //   ]
+  // },
+  // async rewrites() {
+  //   return {
+  //     beforeFiles: [
+  //       {
+  //         source: '/proxy-api/:path*',
+  //         destination: 'https://api.mangadex.org/:path*',
+  //       },
+  //     ],
+  //   }
+  // },
 }
 
 module.exports = nextConfig
